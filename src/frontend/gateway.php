3a65ec8b8e46d1e1712de461e436806636301c03
Aidan Cornelius-Bell <aidancornelius@gmail.com>
2011-08-17 12:59:33 +0930

    Changed Title / Links
    Renamed project masthead and removed redundant links

--- src/frontend/gateway.php
+++ src/frontend/gateway.php
@@ -112,7 +112,7 @@ body,td,th {
 </style>
 </head>
 <body>
-<div id="header"><div id="presenter"><div style="float:left;">Leading Potential - Generative Leadership Development Center</div> <div style="float:right;"><a href="index.php">Home</a> <a href="about.php">About</a> <a href="http://beaconsfieldit.us2.list-manage.com/subscribe?u=2a83f10ae5ed89247e6ada7b2&id=b7e3c30bb7">Tester Registration</a> <a href="login.php?developer=Tester">Tester Login</a> <a href="login.php?developer=Developer">Developer Login</a></div></div></div>
+<div id="header"><div id="presenter"><div style="float:left;">Leading Potential - Generative Leadership Program</div> <div style="float:right;"><a href="index.php">Home</a> <a href="about.php">About</a> <a href="http://beaconsfieldit.us2.list-manage.com/subscribe?u=2a83f10ae5ed89247e6ada7b2&id=b7e3c30bb7">Tester Registration</a>  <a href="login.php?developer=Developer">Developer Login</a></div></div></div>
 <div id="container">Err.</div>
 <div id="footer"><div style="float:left;">&copy; 2011 <a href="http://www.leadingpotential.com.au">Leading Potential</a>. A <a href="http://www.beaconsfieldit.net">Beaconsfield IT</a> project.</div></div>
 </body>
